dfc7c4a8cf6b47f3e35802def026da4b5bd170a8
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-02-01 16:24:38 -0600

    initial commit

---
+++ casino.js
@@ -0,0 +1,43 @@
+const prompt =require('prompt-sync')();
+
+const flip = Math.random() < 0.5 ? 'H' : 'T';
+
+let balance = 10
+
+function coinFlip(flipQ) {
+    return flipQ === flip;
+}
+
+function addBet(bet) {
+    return balance += Number(bet);
+}
+
+function subtractBet(bet) {
+    return balance -= Number(bet);
+}
+
+do {
+    let bet = prompt("Hello player! How much would you like to bet? ");
+
+    console.log(`${bet}`);
+
+    let flipQ = prompt("Heads or Tails? (Type 'H' or 'T') ");
+
+    console.log(`You chose ${flipQ}`);
+    console.log(`The house flips ${flip}`);
+    
+    if (coinFlip === true) {
+        addBet(bet);
+        console.log(`Congratulations! You won the bet and ${bet} dollars has been added to your balance.`)
+    } else {
+        subtractBet(bet);
+        console.log(`So sorry! You lost the bet and ${bet} dollars has been subtracted from your balance.`)
+    }
+
+    console.log(`Your balance is ${balance}`);
+
+} while (balance > 0);
+
+if (balance <= 0) {
+console.log("You are out of money. The house always wins!");
+}
---
+++ shapefunctions.js
@@ -0,0 +1,26 @@
+function getAreaOfCircle(radius) {
+    return 3.14 * radius * radius;
+}
+
+console.log(getAreaOfCircle(4))
+
+
+function getCircumferenceOfCircle(radius) {
+    return 2 * 3.14 * radius;
+}
+
+console.log(getCircumferenceOfCircle(10))
+
+
+function getAreaOfSquare(side) {
+    return side * side;
+}
+
+console.log(getAreaOfSquare(4))
+
+
+function getAreaOfTriangle(base, height) {
+    return 1/2 * height * base;
+}
+
+console.log(getAreaOfTriangle(4, 5))
