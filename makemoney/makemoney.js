f1e1f1a88bcf03efc719eb692f092dd31e7f4035
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-02-22 16:02:18 -0600

    new divs

--- makemoney/makemoney.js
+++ makemoney/makemoney.js
@@ -1,14 +1,29 @@
+//when clicking submit button, read data from parts of form, then create an element utilizing data from form (or pull from pre-existing elements, hidden, then clone them?)
+
 (function () {
 
-//data from form
 
-const getNumber = document.getElementById("howmany")
-const getCoin = document.getElementById("whichcoin")
-const makeMoney = document.getElementById("makemoney")
+const makeMoney = document.getElementById("makemoney");
+const getNumber = document.getElementById("howmany");
+const getCoin = document.getElementById("whichcoin");
+
+makeMoney.addEventListener("click", () => {
+
+    
+
+// const newPenny =
+// const newNickel=
+// const newDime =
+// const newQuarter =
+
+    // output.append(newPDiv)
+    // output.append(newNDiv)
+    // output.append(newDDiv)
+    // output.append(newQDiv)
+})
+
 
 
 
 }());
-
-//when clicking submit button, read data from parts of form, then create an element utilizing data from form
 
