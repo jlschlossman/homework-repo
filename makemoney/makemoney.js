fc1093d637a4f51e027b1169ac7b7eecc156d33d
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-02-22 14:57:14 -0600

    added varaibales for data

---
+++ makemoney/makemoney.js
@@ -0,0 +1,14 @@
+(function () {
+
+//data from form
+
+const getNumber = document.getElementById("howmany")
+const getCoin = document.getElementById("whichcoin")
+const makeMoney = document.getElementById("makemoney")
+
+
+
+}());
+
+//when clicking submit button, read data from parts of form, then create an element utilizing data from form
+
