bab1d18ddf119e3ae00aba47d3b6b46c96e1ca14
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-03-20 15:41:58 -0500

    starting over

--- piglatin/piglatin.js
+++ piglatin/piglatin.js
@@ -2,17 +2,3 @@
     If a word begins with a vowel, just add "yay" to the end. For example, "out" is translated into "outyay".
     If it begins with a consonant, then we take all consonants before the first vowel and we put them on the end of the word. For example, "which" is translated into "ichwhay".
 */
-
-
-function translate(word) {
-    let wordPigLatin = word;
-    for (let i = 0; i <= wordPigLatin.length; i++) {
-        if (i === "a" || "e" || "i" || "o" || "u") {
-            if (i === [0]) {
-                wordPigLatin.push("yay");
-                return wordPigLatin;
-            }
-        }
-    }
-}
-console.log(translate("out"));
